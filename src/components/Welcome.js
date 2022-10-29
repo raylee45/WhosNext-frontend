dc0288ee6a9ded0e41708824f16d6289a0a4adcf
Jared Hayes <jhayes907@gmail.com>
2022-10-29 14:05:19 -0500

    Feat: added jaredFeat branch and npm installed react-bootstrap

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+
 const Welcome = () => {
     return (
+        <>
         <div>
-            <h1>MERN Authentication</h1>
-            <p>Welcome to my app.</p>
+            <h1>WhosNext</h1>
+            <p>Dating for the shallow end of the pool.</p> 
         </div>
+        <Button variant="primary">Get Started</Button>{' '}
+        </>
     )
 }
 
